977efd6f89a6a331952a19d75e0df2c3f306ede0
David Umoru <umorudavido@gmail.com>
2023-10-01 15:14:23 +0100

    Implement file saving

---
+++ src/utils/file.utils.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const videoUploadDir = path.join(__dirname, '../public/videos');
+
+exports.saveFile = (file) => {
+  return new Promise((resolve, reject) => {
+    const filePath = path.join(videoUploadDir, file.originalname);
+    const fileStream = fs.createWriteStream(filePath);
+
+    fileStream.on('error', (error) => {
+      reject(error);
+    });
+
+    fileStream.on('finish', () => {
+      resolve(filePath);
+    });
+
+    fileStream.end(file.buffer);
+  });
+};
