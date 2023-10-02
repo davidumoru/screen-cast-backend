ddc4b5e778d9ca33d5f1b507a5a1ad7f5d39eb85
David Umoru <umorudavido@gmail.com>
2023-10-02 12:27:39 +0100

    Refactor routes

--- src/routes/transcription.routes.js
+++ src/routes/transcription.routes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const transcriptionController = require('../controllers/transcription.controllers');
 
-// Create a transcription
-router.post('/create', transcriptionController.createTranscription);
-
-// Get transcriptions by video ID
-router.get('/:videoId', transcriptionController.getTranscriptionByVideoId);
+router.post("/create/:videoId", transcriptionController.transcribeVideo);
+router.get("/:videoId", transcriptionController.getTranscription);
 
 module.exports = router;
