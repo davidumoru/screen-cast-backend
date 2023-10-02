a0ac08a4944dc0f2f821df7457dd3e14a2d1c104
David Umoru <umorudavido@gmail.com>
2023-10-02 12:43:50 +0100

    Remove get by id

--- src/routes/transcription.routes.js
+++ src/routes/transcription.routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const transcriptionController = require('../controllers/transcription.controllers');
 
 router.post("/create/:videoId", transcriptionController.transcribeVideo);
-router.get("/:videoId", transcriptionController.getTranscription);
 
 module.exports = router;
