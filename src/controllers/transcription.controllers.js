7dfe7528f72bcd122b4ceb339242f79a574e66d0
David Umoru <umorudavido@gmail.com>
2023-10-02 12:45:13 +0100

    Implement function to transcribe video

--- src/controllers/transcription.controllers.js
+++ src/controllers/transcription.controllers.js
@@ -1,23 +1,23 @@
-const Transcription = require('../models/transcription.models');
+const fs = require("fs");
+const Video = require("../models/video.models");
+const openaiUtils = require("../utils/openai.utils");
 
-// Handle transcription creation
-exports.createTranscription = async (req, res) => {
+// Handle video transcription
+exports.transcribeVideo = async (req, res) => {
   try {
+    const video = await Video.findById(req.params.videoId);
+    if (!video) {
+      return res.status(404).json({ error: "Video not found" });
+    }
 
-    res.status(201).json({ message: 'Transcription created successfully' });
+    const videoPath = video.filePath;
+
+    // Transcribe the video using OpenAI Whisper
+    const transcription = await openaiUtils.transcribeVideo(videoPath);
+
+    res.status(200).json({ transcription: transcription.text });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-// Retrieve transcription by video ID
-exports.getTranscriptionByVideoId = async (req, res) => {
-  try {
-
-    res.status(200).json(transcriptions);
-  } catch (error) {
-    console.error(error);
-    res.status(404).json({ error: 'Transcriptions not found' });
+    res.status(500).json({ error: "Internal server error" });
   }
 };
