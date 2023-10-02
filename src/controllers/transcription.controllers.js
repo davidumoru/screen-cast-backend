f77d7c0fc4f8461ea88f2abfc005334a6599a36f
David Umoru <umorudavido@gmail.com>
2023-10-02 12:47:00 +0100

    Save transcript to db

--- src/controllers/transcription.controllers.js
+++ src/controllers/transcription.controllers.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const Video = require("../models/video.models");
 const openaiUtils = require("../utils/openai.utils");
+const Transcription = require("../models/transcription.models");
 
 // Handle video transcription
 exports.transcribeVideo = async (req, res) => {
@@ -14,6 +15,15 @@ exports.transcribeVideo = async (req, res) => {
 
     // Transcribe the video using OpenAI Whisper
     const transcription = await openaiUtils.transcribeVideo(videoPath);
+
+    // Create a new Transcription document
+    const newTranscription = new Transcription({
+      videoId: video._id,
+      text: transcription.text,
+    });
+
+    // Save the transcription to the database
+    await newTranscription.save();
 
     res.status(200).json({ transcription: transcription.text });
   } catch (error) {
